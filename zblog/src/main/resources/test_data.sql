af4b6144a47e7088588e16d338d16597bf9102a8
javenock <simiyuenock1990@gmail.com>
2019-02-26 12:46:26 -0800

    update

--- zblog/src/main/resources/test_data.sql
+++ zblog/src/main/resources/test_data.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE zblog;
 insert into role VALUES (1,'ROLE_ADMIN');
 
 insert into users (id, email, enabled, password, username) values (1,'simiyu@gmail.com','true','$2a$11$LhUT8JM5YZHKrgVonqnMrOcQpR3Dz0Ll05RIXUWfP3MNuAwTizuxi','javenock');
